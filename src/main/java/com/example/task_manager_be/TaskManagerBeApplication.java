1a74edf0d7461be2645add776e3e2e4860bece8c
jelena-pavkovic <jelena.pavkovic28@gmail.com>
2026-06-13 15:34:23 +0200

    Initial commit.

---
+++ src/main/java/com/example/task_manager_be/TaskManagerBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_manager_be/TaskManagerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

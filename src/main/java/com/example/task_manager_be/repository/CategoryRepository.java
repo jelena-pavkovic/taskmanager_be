f53be4c2e4b1807bb9dd0662086aaddc92c7b804
jelena-pavkovic <jelena.pavkovic28@gmail.com>
2026-06-25 22:01:52 +0200

    Repositories

--- src/main/java/com/example/task_manager_be/repository/CategoryRepository.java
+++ src/main/java/com/example/task_manager_be/repository/CategoryRepository.java
@@ -2,6 +2,13 @@ package com.example.task_manager_be.repository;
 
 import com.example.task_manager_be.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Modifying
+    @Query(value = "DELETE FROM task_categories WHERE category_id = :categoryId", nativeQuery = true)
+    void deleteTaskCategoriesByCategoryId(@Param("categoryId") Long categoryId);
 }
--- src/main/java/com/example/task_manager_be/repository/TaskRepository.java
+++ src/main/java/com/example/task_manager_be/repository/TaskRepository.java
@@ -2,6 +2,9 @@ package com.example.task_manager_be.repository;
 
 import com.example.task_manager_be.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +13,7 @@ public interface TaskRepository  extends JpaRepository<Task, Long> {
 
     List<Task> findByAssigneeId(Long userId);
 
+    @Modifying
+    @Query("UPDATE Task t SET t.assignee = null WHERE t.assignee.id = :userId")
+    void unassignUser(@Param("userId") Long userId);
 }
